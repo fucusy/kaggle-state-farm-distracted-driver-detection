99685ff31e0d53a9add16f8806949c3f36bcfb3f
fucusy <fucus@qq.com>
2016-05-27 09:02:23 +0800

    add config, ignore

--- config.tmpl.py
+++ config.tmpl.py
@@ -4,14 +4,17 @@ import model.model as model
 
 class Project:
 
+    # required, your project's absolute path, in other way, it's the absolute path for this file
+    project_path = "/Users/fucus/Documents/buaa/projects/State_Farm_Distracted_Driver_Detection/code/kaggle_distractedDrivers"
+
+    driver_img_list_path = ""
+
     # required, this path contain the train image in sub-folder, the there are ten sub-folders, c0, c1, c2, c3 .... c9
     train_img_folder_path = "/Users/fucus/Documents/buaa/projects/State_Farm_Distracted_Driver_Detection/data/imgs/train"
 
     # required, this path contain imgs to be tested
     test_img_folder_path = "/Users/fucus/Documents/buaa/projects/State_Farm_Distracted_Driver_Detection/data/imgs/test"
 
-    # required, your project's absolute path, in other way, it's the absolute path for this file
-    project_path = "/Users/fucus/Documents/buaa/projects/State_Farm_Distracted_Driver_Detection/code/kaggle_distractedDrivers"
 
     # not required, a img path for you exercise program
     test_img_example_path = "/Users/fucus/Documents/buaa/projects/State_Farm_Distracted_Driver_Detection/data/imgs/train/c0/img_27327.jpg"
@@ -24,3 +27,6 @@ class Project:
 
     # required, save cache or not
     save_cache = False
+
+    # 
+    keras_train_weight = "%s/CNN/vgg_try_karea/cache/model_weights_2_vgg_16_2x20.h5" % project_path 
