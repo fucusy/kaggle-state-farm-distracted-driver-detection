3a08f30001f2a651ac2e2788fbd1f6e5d6874450
fucusy <fucus@qq.com>
2016-06-03 12:11:33 +0800

    mv config.py content to config.tmpl.py

--- config.tmpl.py
+++ config.tmpl.py
@@ -19,7 +19,7 @@ class Project:
     test_img_example_path = "/Users/fucus/Documents/buaa/projects/State_Farm_Distracted_Driver_Detection/data/imgs/train/c0/img_27327.jpg"
 
     # required, predict model
-    predict_model = model.RandomForestClassification()
+    predict_model = RandomForestClassification()
 
     # required, result output path
     result_output_path = "./result/"
@@ -30,7 +30,7 @@ class Project:
 
 class CNN:
     #
-    keras_train_weight = "%s/CNN/vgg_try_karea/cache/model_weights_2_vgg_16_2x20.h5" % project_path
+    keras_train_weight = "%s/CNN/vgg_try_karea/cache/model_weights_2_vgg_16_2x20.h5" % Project.project_path
 
 
     # keras structure files
@@ -38,3 +38,43 @@ class CNN:
     vgg_weight_file_path = "/home/chenqiang/kaggle_driver_data/vgg16_weights.h5"
 
     fine_tuning_vgg_weight_file_path = ""
+
+    model_name = 'vgg_self'
+
+    '''/path/to/json/vgg_self_exp1_keras_arch.json'''
+    model_arch_file_name = '/home/liuzheng/competition/kaggle/distractedDrivers/vgg_self_exp1_keras_arch.json'
+
+    '''/path/to/h5/vgg16_weights.h5'''
+    model_weights_file_name = '/home/liuzheng/competition/kaggle/distractedDrivers/vgg_self_exp1_keras_weights_best_vLoss0.11026_vAcc0.969.h5'
+
+    optimizer = 'sgd'
+
+    train_iter = 50
+
+    test_batch_size = 64
+
+    '''path/to/save/model/'''
+    model_save_path = '/home/liuzheng/competition/kaggle/distractedDrivers/'
+
+    '''path/to/prediction/and/file/name/'''
+    prediction_save_file = '/home/liuzheng/competition/kaggle/distractedDrivers/prediction.csv'
+
+    feature_save_path = '/home/liuzheng/competition/kaggle/distractedDrivers/'
+
+class Data:
+
+    img_size = [3, 224, 224]
+
+    mean_image_file_name = '/home/liuzheng/competition/kaggle/distractedDrivers/imgs/meanImage.npy'
+
+    image_crop = 'entire'# whether random crop images or not
+
+    color_mode = 'rgb'# 'gray'
+
+    fragment_size = 2048
+
+    batch_size = 32
+
+    class_num = 10
+
+    validation_split = 0.2
