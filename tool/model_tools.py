61d88421c91cde70c251f44e8096a9b780315501
fucusy <fucus@qq.com>
2016-06-04 09:10:51 +0800

    uncouple dataset and kerasmodel

--- tool/model_tools.py
+++ tool/model_tools.py
@@ -17,7 +17,7 @@ from keras.callbacks import ModelCheckpoint
 
 from keras.layers.advanced_activations import LeakyReLU, PReLU
 from keras.layers.normalization import BatchNormalization
-
+import logging
 import numpy as np
 import csv
 import skimage.io as skio
@@ -189,39 +189,28 @@ class KerasFeatureExtractor(object):
 
 
 class KerasModel(object):
-    def __init__(self, data_set):
+    def __init__(self, cnn_model):
 
-        model_name = config.CNN.model_name,
-        test_batch_size = config.CNN.test_batch_size,
-        n_iter = config.CNN.train_iter,
-        model_arch_file = config.CNN.model_arch_file_name,
-        model_weights_file = config.CNN.model_weights_file_name,
-        model_save_path = config.CNN.model_save_path,
+        model_name = config.CNN.model_name
+        test_batch_size = config.CNN.test_batch_size
+        n_iter = config.CNN.train_iter
+        model_arch_file = config.CNN.model_arch_file_name
+        model_weights_file = config.CNN.model_weights_file_name
+        model_save_path = config.CNN.model_save_path
         prediction_save_file = config.CNN.prediction_save_file
 
 
         self._model_name = model_name
-        self._data_set = data_set
-        # self._data_set._batch_size = batch_size
         self._n_iter = n_iter
         self._model_arch_file = model_arch_file
         self._model_weights_file = model_weights_file
         self._model_save_path = model_save_path
-        if model_name == 'vgg_keras':
-            img_size = data_set.get_img_size()
-            self._model = vgg_std16_model(img_rows=img_size[1], img_cols=img_size[2], color_type=3,
-                                          model_weights_file=model_weights_file, continueFile='')
-        elif model_arch_file:
-            self._model = model_from_json(open(self._model_arch_file).read())
-            self._model.load_weights(self._model_weights_file)
-            print('compiling model %s.....'%(self._model_name))
-            self._model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
-        else:
-            print('Warning: Model Architecture is not defined!')
-        
+        self._batch_size = config.CNN.batch_size
         self._test_batch_size = test_batch_size
+
         self._prediction_save_file = prediction_save_file
         self._prediction = {}
+        self._model = cnn_model
     
     def set_model_arch(self, model_arch):
         self._model = model_arch
@@ -236,69 +225,92 @@ class KerasModel(object):
     def save_model_weights(self, weights_path_file, overwrite=True):
         self._model.save_weights(weights_path_file, overwrite=overwrite)
     
-    def train_model(self, save_best=True):
+    def train_model(self, train_data, validation_data, save_best=True):
         json_string = self._model.to_json()
-        open(self._model_save_path + self._model_name + 'keras_arch.json', 'w').write(json_string)
-        if self._data_set.need_training_fragment:
+        json_path = os.path.join(self._model_save_path, self._model_name + '.json')
+        open(json_path, 'w').write(json_string)
+
+        fragment_size = config.CNN.load_image_to_memory_every_time
+        if fragment_size > 0:
             '''If data fragment is needed.'''
-            minValidLoss = np.inf
-            maxValidAcc = 0.
+            min_loss = np.inf
+            max_acc = 0.
             
             for it in range(self._n_iter):
                 '''Every iter, we read fragments one by one and train model with them setting nb_epoch=1.'''
-                fragment_count = 0
-                while self._data_set.have_next_training_fragment_reset:
-                    fragment_count += 1
-                    print('%s | iter %03d --> validation fragment %d / %d'%(self._model_name,
-                                           it, fragment_count, self._data_set.get_training_fragment_num()))
-                    x_train, y_train = self._data_set.next_training_fragment(validation_flag=False)
-                    self._model.fit(x_train, y_train, batch_size=self._data_set._batch_size, nb_epoch=1)
+                image_count = 0
+
+                # set index to zero, prepare for have_next function
+                train_data.reset_index()
+
+                while train_data.have_next():
+                    x_train, y_train, _ = train_data.next_fragment(fragment_size,need_label=True)
+                    image_count += fragment_size
+                    logging.info('%s | iter %03d --> validation fragment %d / %d'
+                                   % (self._model_name, it, image_count, train_data.count()))
+
+                    self._model.fit(x_train, y_train, batch_size=self._batch_size, nb_epoch=1)
                 ''' After all training fragments are trained, we read validation fragments and evaluate the model on them. '''
-                evaLoss = []
-                evaAcc = []
-                fragment_count = 0
-                while self._data_set.have_next_validation_fragment_reset:
-                    fragment_count += 1
-                    print('%s | iter %03d --> validation fragment %d / %d'%(self._model_name,
-                                           it, fragment_count, self._data_set.get_validation_fragment_num()))
-                    x_valid, y_valid = self._data_set.next_training_fragment(validation_flag=True)
-                    loss, acc = self._model.evaluate(x_valid, y_valid, batch_size=self._data_set._batch_size)
-                    evaLoss.append( loss )
-                    evaAcc.append( acc )
+
+                eva_loss = []
+                eva_acc = []
+                image_count = 0
+
+                # set index to zero, prepare for have_next function
+                validation_data.reset_index()
+
+                while validation_data.have_next():
+                    image_count += fragment_size
+                    print('%s | iter %03d --> validation fragment %d / %d'
+                          % (self._model_name, it, image_count, validation_data.count()))
+
+                    x_valid, y_valid, _ = validation_data.next_fragment(fragment_size,need_label=True)
+
+                    loss, acc = self._model.evaluate(x_valid, y_valid, batch_size=self._batch_size)
+                    eva_loss.append(loss)
+                    eva_acc.append(acc)
                 ''' Compute mean evaluation loss and accuracy and output them.'''
-                evaLoss = np.mean(evaLoss)
-                evaAcc = np.mean(evaAcc)
-                print('%s | iter %03d --> validation loss: %f, acc: %f'%(self._model_name, it+1, evaLoss, evaAcc))
+                eva_loss = np.mean(eva_loss)
+                eva_acc = np.mean(eva_acc)
+                print('%s | iter %03d --> validation loss: %f, acc: %f'%(self._model_name, it, eva_loss, eva_acc))
                 ''' Save the best model. '''
-                if evaLoss < minValidLoss and save_best:
-                    if os.path.exists(self._model_save_path + self._model_name + 'keras_weights_best_vLoss%.5f_vAcc%.3f.h5'%(minValidLoss,maxValidAcc)):
-                        os.remove(self._model_save_path + self._model_name + 'keras_weights_best_vLoss%.5f_vAcc%.3f.h5'%(minValidLoss,maxValidAcc))
-                    minValidLoss = evaLoss
-                    maxValidAcc = evaAcc
-                    self._model.save_weights(self._model_save_path + self._model_name + 'keras_weights_best_vLoss%.5f_vAcc%.3f.h5'%(minValidLoss,maxValidAcc), overwrite=True)
-            
-            self._model.save_weights(self._model_save_path + self._model_name + 'keras_weights_final.h5', overwrite=True)
+                if eva_loss < min_loss and save_best:
+                    old_weight_path = os.path.join(self._model_save_path, self._model_name + 'keras_weights_best_vLoss%.5f_vAcc%.3f.h5'%(min_loss,max_acc))
+                    if os.path.exists(old_weight_path):
+                        os.remove(old_weight_path)
+                    min_loss = eva_loss
+                    max_acc = eva_acc
+                    new_weight_path = os.path.join(self._model_save_path, self._model_name + 'keras_weights_best_vLoss%.5f_vAcc%.3f.h5'%(min_loss,max_acc))
+                    self._model.save_weights(new_weight_path, overwrite=True)
+            final_weight_path = os.path.join(self._model_save_path, self._model_name + 'keras_weights_final.h5')
+            self._model.save_weights(final_weight_path, overwrite=True)
             
         else:
             ''' If no need to fragment, load all training images and train the model directly.'''
-            print('start training')
+            logging.info('start training')
             CallBacks = []
             if save_best:
-                checkpoint = ModelCheckpoint(self._model_save_path + self._model_name + 'keras_checkpoint_weights_best.h5',
-                                            monitor='val_loss', verbose=0, save_best_only=True, mode='auto')
+                weight_path = os.path.join(self._model_save_path, self._model_name + 'keras_checkpoint_weights_best.h5')
+                checkpoint = ModelCheckpoint(weight_path, monitor='val_loss', verbose=0, save_best_only=True, mode='auto')
                 CallBacks.append(checkpoint)
 
-            x_train, y_train = self._data_set.load_traininig_data()
-            self._history = self._model.fit(x_train, y_train, batch_size=self._data_set._batch_size, nb_epoch=self._n_iter,
-                                            validation_split=self._data_set._validation_split, callbacks=CallBacks)
+            x_train, y_train, _ = train_data.load_all_image(need_label=True)
+            x_vali, y_vali, _ = validation_data.load_all_image(need_label=True)
 
-    def predict_model(self):
-        fragment_count = -1
-        while self._data_set.have_next_testing_fragment_reset:
-            fragment_count += 1
+            self._history = self._model.fit(x_train, y_train, batch_size=self._batch_size, nb_epoch=self._n_iter,
+                                             validation_data=(x_vali, y_vali), callbacks=CallBacks)
+
+    def predict_model(self, test_data):
+        image_count = 0
+        fragment_size = config.CNN.load_image_to_memory_every_time
+
+        while test_data.have_next():
+            image_count += fragment_size
             print('%s | --> testing fragment %d / %d'%(self._model_name,
-                                   fragment_count, self._data_set.get_testing_fragment_num()))
-            x_test, name_list = self._data_set.next_testing_fragment()
+                                   image_count, test_data.count()))
+
+
+            x_test, name_list = test_data.next_fragment(fragment_size, need_label=False)
             frag_prediction = self._model.predict_proba(x_test, batch_size=self._test_batch_size)
             self.stat_prediction(frag_prediction, name_list)
         ''' We still call fuse function if only one prediction per image for universality.
@@ -309,43 +321,31 @@ class KerasModel(object):
         self.save_prediction()
 
     ''' Update prediction dict. '''
-    def  stat_prediction(self, frag_prediction, frag_list):
+    def stat_prediction(self, frag_prediction, frag_list):
         for i in range(frag_prediction.shape[0]):
-            imgName = frag_list[i][:frag_list[i].rfind('_')]
-            if imgName in self._prediction.keys():
-                self._prediction[imgName].append(frag_prediction[i, :])
+            img_name = frag_list[i]
+            if img_name in self._prediction.keys():
+                self._prediction[img_name].append(frag_prediction[i, :])
             else:
-                self._prediction[imgName] = [frag_prediction[i, :]]
+                self._prediction[img_name] = [frag_prediction[i, :]]
 
     ''' In case some methods give multiple predictions to a testing image, this function fuse all predictions into one. '''
     def fuse_prediction(self):
         finalDict = {}
         for k in self._prediction.keys():
-            finalDict[k] = np.mean(np.array(self._prediction[k]), axis=0)
-        self._prediction = finalDict
+            self._prediction[k] = np.mean(np.array(self._prediction[k]), axis=0)
+
 
     ''' Save prediction dict '''
     def save_prediction(self):
-        fileObj = open(self._prediction_save_file, 'wb')
-        writer = csv.writer(fileObj)
+        file_obj = open(self._prediction_save_file, 'wb')
+        writer = csv.writer(file_obj)
         writer.writerow(['img','c0','c1','c2','c3','c4','c5','c6','c7','c8','c9'])
         keys = self._prediction.keys()
         keys.sort()
         for k in keys:
-            line = [k+'.jpg']
+            line = [k]
             for i in range(10):
-                line.append('%.3f'%(self._prediction[k][i]))
+                line.append('%.3f' % (self._prediction[k][i]))
             writer.writerow(line)
-        fileObj.close()
-
-
-
-
-
-
-
-
-
-
-
-
+        file_obj.close()
