91d36c5e7b2c32fac2ad243c950d9f86256efa5b
fucusy <fucus@qq.com>
2016-07-03 16:10:23 +0800

    add input 120*160, vgg like model

--- model/cnn_model.py
+++ model/cnn_model.py
@@ -23,6 +23,79 @@ import os
 from tool.keras_tool import load_model
 logger = logging.getLogger('cnn_model')
 
+def simple_cnn_vgg_like(lr=1e-3, weights_path=None):
+    img_rows, img_cols, color_type = 120, 160, 3
+    # standard VGG16 network architecture
+    
+    if weights_path is not None and os.path.exists(weights_path):
+        logger.debug("load weigth from fine-tuning weight %s" % weights_path)
+        model = load_model(weights_path)
+    else:
+        model = Sequential()
+        model.add(ZeroPadding2D((1, 1), input_shape=(color_type,
+                                                     img_rows, img_cols)))
+        model.add(Convolution2D(64, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(64, 3, 3, activation='relu'))
+        model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(128, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(128, 3, 3, activation='relu'))
+        model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(256, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(256, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(256, 3, 3, activation='relu'))
+        model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    
+        model.add(Flatten())
+        model.add(Dense(4096, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(4096, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(1000, activation='relu'))
+        model.add(Dropout(0.5))
+
+        # replace more fc layer
+        model.add(Dense(10, activation='softmax'))
+
+        # load the weights
+        
+        f = h5py.File(config.Project.vgg_weight_file_path)
+        for k in range(32):
+            g = f['layer_{}'.format(k)]
+            weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
+            model.layers[k].set_weights(weights)
+            logger.info("set %d layer weight to %s" % (k, weights))
+        f.close()
+        logger.debug("load part weigth from vgg weight")
+        logger.debug('Model loaded.')
+
+    sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy',  metrics=['accuracy'])
+
+    return model
 
 def simple_cnn_for_test(lr=1e-3, weights_path=None):
     img_rows, img_cols, color_type = 480, 640, 3
