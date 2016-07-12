c91a30fb4f755c584484e6365f7b0043ab7e4284
fucusy <fucus@qq.com>
2016-07-12 15:18:54 +0800

    add add_padding function

--- preprocess/resize.py
+++ preprocess/resize.py
@@ -12,6 +12,7 @@ import shutil
 from tool.keras_tool import load_image_path_list
 from preprocess.argument import loop_process_train_image
 from preprocess.argument import loop_process_test_image
+import numpy as np
 
 '''
     Save all resized images to training and testing folders.
@@ -21,6 +22,18 @@ from preprocess.argument import loop_process_test_image
 def resize_image(img, img_size):
     return sktr.resize(img, output_shape=img_size)
 
+
+def add_padding(img, img_size):
+    result = np.zeros((img_size[0], img_size[1], 3), dtype=np.uint8)
+    origin_size = img.shape
+    offset_height = (img_size[0] - origin_size[0]) / 2
+    offset_width = (img_size[1] - origin_size[1]) / 2
+
+    for w in range(origin_size[0]):
+        for h in range(origin_size[1]):
+            result[w + offset_height, h + offset_width, :] = img[w, h, :]
+
+    return result
 
 def resize_image_main(from_path, img_size, force=False):
     for path in from_path:
@@ -39,4 +52,7 @@ if __name__ == "__main__":
     level = logging.DEBUG
     FORMAT = '%(asctime)-12s[%(levelname)s] %(message)s'
     logging.basicConfig(level=level, format=FORMAT, datefmt='%Y-%m-%d %H:%M:%S')
-    resize_image_main()
+    driver = skio.imread(config.Project.test_img_example_path)
+    to_size = (driver.shape[0] + 100, driver.shape[1] + 100)
+    padding = add_padding(driver, to_size)
+    skio.imsave("add_padding.jpg", padding)
